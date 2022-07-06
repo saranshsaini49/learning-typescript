8b62384efdcbf774899b9b2c32fec4300f6efbff
Saransh Saini <saranshsss99@gmail.com>
2022-07-06 18:48:44 +0530

    tuples completed

---
+++ src/ch-04-tuples/index.ts
@@ -0,0 +1,6 @@
+//tuples can have multiple types of data
+let tup: [number, boolean, string] = [5, true, "JOD"];
+//the order matters in our tuple and will throw an error.
+
+//named tuples
+const graph: [x: number, y: number] = [55.2, 41.3];
